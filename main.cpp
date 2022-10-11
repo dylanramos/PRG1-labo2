03292735b4229d67c4b1d52704060be60e61eee5
dylanramos <dylan.oliveiraramos@heig-vd.ch>
2022-10-11 17:32:07 +0200

    eval

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@
 using namespace std;
 
 int main() {
-    cout << "Quel uber? Entrez 1 pour X, 2 pour POP ou 3 pour BLACK" << endl;
+    cout << "Quel uber? Entrez 1 pour X, 2 pour POP, 3 pour BLACK ou 4 pour ULTIMATE" << endl;
     int uberType;
     cin >> uberType;
 
-    while (uberType < 1 || uberType > 3) {
+    while (uberType < 1 || uberType > 4) {
         cout << "Ce type de uber n'existe pas" << endl;
         cin >> uberType;
     }
@@ -35,9 +35,10 @@ int main() {
 
     cout << endl; // Blank space before the bill
 
-    const double UBER_VALUES[3][5] = {{3.0, 1.8,  0.3, 6.0,  6.0},
-                                      {3.0, 1.35, 0.3, 6.0,  6.0},
-                                      {8.0, 3.6,  0.6, 15.0, 10.0}};
+    const double UBER_VALUES[4][5] = {{3.0, 1.8,  0.3, 6.0,  6.0},
+                                      {5.0, 1.35, 0.3, 8.0,  10.0},
+                                      {8.0, 3.6,  0.6, 15.0, 10.0},
+                                      {10.0, 4.9, 0.9, 20.0, 25.0}};
 
     --uberType; // To match with the array of uber values
 
@@ -48,11 +49,11 @@ int main() {
     minutes = ceil(minutes);
     kilometers = round(kilometers * 10) / 10; // Round to the nearest 100 meters
 
-    const int TVA = 8;
+    const double TVA = 7.7;
     double distancePrice, timePrice, total, price, tvaPrice;
 
-    distancePrice = round(kilometerPrice * kilometers * 100) / 100,
-    timePrice = minutePrice * minutes,
+    distancePrice = round(kilometerPrice * kilometers * 20) / 20, // Round to nearest .05
+    timePrice = round(minutePrice * minutes * 20) / 20,
     total = basePrice + distancePrice + timePrice;
     price = total;
 
@@ -92,11 +93,11 @@ int main() {
         }
     }
 
-    ++rightColumnSize; // + 1 whitespace
+    rightColumnSize += 3; // + 3 whitespaces
 
     // Find the longest string to size the left column of the bill
     const array<string, 10> BILL_LABELS = {"Frais annulation", "distance", "temps ecoule", "Prix de base",
-                                           "Prix distance", "Prix temps", "Total", "Course mimimale", "Prix",
+                                           "Prix distance", "Prix temps", "Total", "Course minimale", "Prix",
                                            "(incl. TVA)"};
     int leftColumnSize = 0;
 
@@ -106,40 +107,38 @@ int main() {
         }
     }
 
-    ++leftColumnSize; // + 1 whitespace
+    leftColumnSize += 5; // + 5 whitespaces
 
-    const char BILL_CORNER_CHAR = '+', BILL_HORIZONTAL_CHAR = '-', BILL_VERTICAL_CHAR = '|', BILL_SEPARATOR_CHAR = ':';
-    const array<string, 3> UBER_NAMES = {"uberX", "uberPOP", "uberBLACK"};
+    const char BILL_CORNER_CHAR = ' ', BILL_HEAD_CHAR = 'v', BILL_FOOT_CHAR = '^', BILL_LEFT_CHAR = '>',
+            BILL_RIGHT_CHAR = '<', BILL_SEPARATOR_CHAR = ':';
+    const array<string, 4> UBER_NAMES = {"uberX", "uberPOP", "uberBLACK", "uberULTRA"};
 
     string uberName = UBER_NAMES[uberType];
 
     int billWidth = leftColumnSize + rightColumnSize + 3; // 1 for the separator and 2 for the whitespaces
     int widthForCenter = (billWidth / 2 + uberName.length() / 2) + 1; // To center the title, + 1 is for the left border
 
-    // Store redundant strings in variables
-    ostringstream horizontalBorder, sectionSeparator;
+    // Store redundant string in variable
+    ostringstream sectionSeparator;
 
-    horizontalBorder << BILL_CORNER_CHAR << setw(billWidth) << setfill(BILL_HORIZONTAL_CHAR)
-                     << BILL_CORNER_CHAR << endl;
-    sectionSeparator << BILL_VERTICAL_CHAR << setw(billWidth) << setfill(' ') << BILL_VERTICAL_CHAR
-                     << endl;
+    sectionSeparator << BILL_LEFT_CHAR << setw(billWidth) << setfill(' ') << BILL_RIGHT_CHAR << endl;
 
     // Header
-    cout << horizontalBorder.str();
+    cout << BILL_CORNER_CHAR << setw(billWidth) << setfill(BILL_HEAD_CHAR) << BILL_CORNER_CHAR << endl;
     cout << sectionSeparator.str();
-    cout << BILL_VERTICAL_CHAR << setw(widthForCenter) << uberName << setw(billWidth - widthForCenter)
-         << BILL_VERTICAL_CHAR << endl;
+    cout << BILL_LEFT_CHAR << setw(widthForCenter) << setfill(' ') << uberName
+         << setw(billWidth - widthForCenter) << BILL_RIGHT_CHAR << endl;
     cout << sectionSeparator.str();
 
     if (isCanceled) {
-        cout << BILL_VERTICAL_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[0] << BILL_SEPARATOR_CHAR
-             << setw(rightColumnSize) << right << billValues[0] << BILL_VERTICAL_CHAR << endl;
+        cout << BILL_LEFT_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[0] << BILL_SEPARATOR_CHAR
+             << setw(rightColumnSize) << right << billValues[0] << BILL_RIGHT_CHAR << endl;
         cout << sectionSeparator.str();
     } else {
         // Distance - Elapsed time - Base price - Distance price - Time price - Total
         for (int i = 1; i <= 6; ++i) {
-            cout << BILL_VERTICAL_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[i]
-                 << BILL_SEPARATOR_CHAR << setw(rightColumnSize) << right << billValues[i] << BILL_VERTICAL_CHAR
+            cout << BILL_LEFT_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[i]
+                 << BILL_SEPARATOR_CHAR << setw(rightColumnSize) << right << billValues[i] << BILL_RIGHT_CHAR
                  << endl;
 
             if (i == 2 || i == 6) cout << sectionSeparator.str();
@@ -147,22 +146,22 @@ int main() {
 
         // Minimum price
         if (total < minimumPrice) {
-            cout << BILL_VERTICAL_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[7]
-                 << BILL_SEPARATOR_CHAR << setw(rightColumnSize) << right << billValues[7] << BILL_VERTICAL_CHAR
+            cout << BILL_LEFT_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[7]
+                 << BILL_SEPARATOR_CHAR << setw(rightColumnSize) << right << billValues[7] << BILL_RIGHT_CHAR
                  << endl;
             cout << sectionSeparator.str();
         }
     }
 
     // Price
-    cout << BILL_VERTICAL_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[8] << BILL_SEPARATOR_CHAR
-         << setw(rightColumnSize) << right << billValues[7] << BILL_VERTICAL_CHAR << endl;
+    cout << BILL_LEFT_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[8] << BILL_SEPARATOR_CHAR
+         << setw(rightColumnSize) << right << billValues[7] << BILL_RIGHT_CHAR << endl;
     // TVA
-    cout << BILL_VERTICAL_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[9] << BILL_SEPARATOR_CHAR
-         << setw(rightColumnSize) << right << billValues[8] << BILL_VERTICAL_CHAR << endl;
+    cout << BILL_LEFT_CHAR << " " << setw(leftColumnSize) << left << BILL_LABELS[9] << BILL_SEPARATOR_CHAR
+         << setw(rightColumnSize) << right << billValues[8] << BILL_RIGHT_CHAR << endl;
     // Footer
     cout << sectionSeparator.str();
-    cout << horizontalBorder.str();
+    cout << BILL_CORNER_CHAR << setw(billWidth) << setfill(BILL_FOOT_CHAR) << BILL_CORNER_CHAR << endl;
 
     return 0;
 }
